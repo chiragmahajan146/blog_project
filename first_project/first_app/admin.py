b51ab7093fbd946f2cc2f4e0ea7b4c52b681c192
chiragmahajan146 <84735247+chiragmahajan146@users.noreply.github.com>
2021-05-25 16:00:12 +0530

    Delete first_project directory

--- first_project/first_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from first_app.models import topic,accessrecord,webpage
-admin.site.register(accessrecord)
-# Register your models here.
-admin.site.register(topic)
-admin.site.register(webpage)
